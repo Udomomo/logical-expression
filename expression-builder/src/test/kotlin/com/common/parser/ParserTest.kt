23efb52a89060fca35ed30a0fc1a855b20618d0f
Udomomo <udomomo@gmail.com>
2025-11-15 12:06:54 +0900

    update .gitignore

--- expression-builder/src/test/kotlin/com/common/parser/ParserTest.kt
+++ expression-builder/src/test/kotlin/com/common/parser/ParserTest.kt
@@ -1,12 +1,13 @@
 package com.common.parser
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class ParserTest {
     @Test
+    @DisplayName("入力した式を正しくパースできること")
     fun `should correctly parse input`() {
-        // Parser.parse()に式を渡し、正しいList<Token>になるかのテストを書く
         val input = "TRUE AND ( FALSE OR NOT TRUE )"
         val expectedTokens = listOf(
             Token.TRUE,
@@ -23,9 +24,27 @@ class ParserTest {
     }
 
     @Test
+    @DisplayName("入力した式で、カッコと値にスペースがなくても正しくパースできること")
     fun `should correctly parse input when parenthesis is not split`() {
-        // Parser.parse()に式を渡し、正しいList<Token>になるかのテストを書く
         val input = "TRUE AND (FALSE OR NOT TRUE)"
+        val expectedTokens = listOf(
+            Token.TRUE,
+            Token.AND,
+            Token.LPAREN,
+            Token.FALSE,
+            Token.OR,
+            Token.NOT,
+            Token.TRUE,
+            Token.RPAREN
+        )
+        val actualTokens = Parser.parse(input)
+        Assertions.assertEquals(actualTokens, expectedTokens)
+    }
+
+    @Test
+    @DisplayName("入力した式が小文字でも正しくパースできること")
+    fun `should correctly parse input when value and operator is lowercase`() {
+        val input = "true and ( false or not true )"
         val expectedTokens = listOf(
             Token.TRUE,
             Token.AND,
